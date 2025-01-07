1f9a2ca214ea8910712853196dd18f1ec72e7115
healy <healy_jared@yahoo.com>
2025-01-07 20:34:31 +0800

    khuop;p

--- client/src/app/shop/page.tsx
+++ client/src/app/shop/page.tsx
@@ -1,8 +1,34 @@
-export default function Home() {
+'use client'
+import React, {useEffect} from "react";
+import {addYapaneseJenForName, getYapaneseJenForName} from "@/components/ServerActions";
+
+
+export default function Shop() {
+    const [yappaneseJen, setYappaneseJen] = React.useState<number>(0);
+    const name: string | null = localStorage.getItem('name')
+    useEffect(() => {
+        if (name) {
+            getYapaneseJenForName(name).then(setYappaneseJen)
+        }
+    })
+
+    function purchase(cost: number) {
+        if (cost < yappaneseJen) {
+            addYapaneseJenForName(name!,cost*-1).then(() => location.reload());
+        } else {
+            alert('broke fuck')
+        }
+    }
+
     return (
         <div style={{height: "100vh"}}>
-            <h1 style={{fontSize: 50}}>Fuckin Shop</h1>
-
+            <img src='/cooler-logo.gif'></img>
+            <h1 style={{position:'absolute',top:'0', right:'0', fontSize:'40px'}}>
+                you got {yappaneseJen} <img style={{display: 'inline'}} src='/jenny.jpeg' width='50' height='50' />
+            </h1>
+            <button onClick={() => purchase(3)}>Buy Le Thing - 3 moneys</button>
+            <p />
+            <button onClick={() => addYapaneseJenForName(name!, 5).then(() => location.reload())}>Get Mulah</button>
         </div>
     );
 }
