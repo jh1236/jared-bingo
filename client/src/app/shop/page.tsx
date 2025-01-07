bff19431658ba7f077a3bc97b6c8d71ba33d7ae7
healy <healy_jared@yahoo.com>
2025-01-07 21:08:00 +0800

    khuop;p

--- client/src/app/shop/page.tsx
+++ client/src/app/shop/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import React, {useEffect} from "react";
 import {addYapaneseJenForName, getYapaneseJenForName} from "@/components/ServerActions";
+import Link from "next/link";
+import classes from "./shop.module.css";
 
 
 export default function Shop() {
@@ -14,21 +16,42 @@ export default function Shop() {
 
     function purchase(cost: number) {
         if (cost <= yappaneseJen) {
-            addYapaneseJenForName(name!,cost*-1).then((newAmount) => setYappaneseJen(newAmount));
+            addYapaneseJenForName(name!, cost * -1).then((newAmount) => setYappaneseJen(newAmount));
         } else {
             alert('broke fuck')
         }
     }
 
     return (
-        <div style={{height: "100vh"}}>
-            <img src='/cooler-logo.gif'></img>
-            <h1 style={{position:'absolute',top:'0', right:'0', fontSize:'40px'}}>
-                you got {yappaneseJen} <img style={{display: 'inline'}} src='/jenny.jpeg' width='50' height='50' />
-            </h1>
-            <button onClick={() => purchase(3)}>Buy Le Thing - 3 moneys</button>
-            <p />
-            <button onClick={() => addYapaneseJenForName(name!, 5).then((newAmount) => setYappaneseJen(newAmount))}>Get Mulah</button>
+        <div>
+            <div style={{height: "200px"}}>
+                <img src='/cooler-logo.gif'></img>
+                <h1 style={{position: 'absolute', top: '96', right: '0', fontSize: '40px'}}>
+                    you got {yappaneseJen} <img style={{display: 'inline'}} src='/jenny.jpeg' width='50' height='50'/>
+                </h1>
+                <Link href='/'>
+                    <button style={{position: 'absolute', top: '96', left: '0', fontSize: '40px'}}>
+                        To BINGO
+                    </button>
+                </Link>
+            </div>
+            <div style={{
+                position: 'absolute', top: '200', left: '0',
+                fontSize: '20px',
+                alignItems: "center",
+                width: '100%',
+                textAlign: "center"
+            }}>
+                <button
+                    className={classes.shopButton}
+                    onClick={() => addYapaneseJenForName(name!, 5).then((newAmount) => setYappaneseJen(newAmount))}>Get
+                    Mulah
+                </button>
+                <br/>
+                <button onClick={() => purchase(3)} className={classes.shopButton}>Buy Le Thing - 3 moneys</button>
+
+            </div>
         </div>
-    );
+    )
+        ;
 }
