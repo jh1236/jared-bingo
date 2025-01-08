19ee31aa863885917cfa818e496c31fe3c692c66
Jared Healy <healy_jared@yahoo.com>
2025-01-08 13:52:51 +0800

    minor bug fix in dict trim

--- client/src/resources/fix_dict.py
+++ client/src/resources/fix_dict.py
@@ -6,7 +6,7 @@ def create_dict():
     with open("dictionary.json") as fp:
         lines: dict[str, str] = json.load(fp)
 
-    out = [i.title() for i in sorted(lines) if i.isalpha() and len(i) > 6 and len(i) < 11]
+    out = [i.title() for i in sorted(lines) if i.isalpha() and 6 < len(i) < 11]
     print(f"The length of the dictionary is {len(out):,} words")
 
     with open("dictionary_keys.json", "w+") as fp:
@@ -17,7 +17,7 @@ def trim_dict():
     with open("dictionary_keys.json") as fp:
         lines: list[str] = json.load(fp)
     ans = ''
-    while ans != 'x':
+    while ans not in ['x', 'exit']:
         i = random.randint(0, len(lines))
         ans = input(f'Should "{lines[i]}" be included in the dictionary? (Y\\N)\t')
         while ans.lower() not in ['y', 'yes', 'n', 'no', 'x', 'exit']:
